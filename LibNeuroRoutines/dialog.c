4911f487af06528cae5a262113e58b817db67de3
HenadziMatuts <eyesscreasmake@rambler.ru>
2018-08-10 15:17:13 +0300

    Zeroize header before building the frame.

--- LibNeuroRoutines/dialog.c
+++ LibNeuroRoutines/dialog.c
@@ -6,6 +6,7 @@ void build_text_frame(uint32_t _h, uint32_t _w, imh_hdr_t *dst)
 	uint8_t *p = (uint8_t*)dst + sizeof(imh_hdr_t);
 	uint32_t w = _w / 2, h = _h;
 
+	memset(dst, 0, sizeof(imh_hdr_t));
 	dst->height = _h;
 	dst->width = _w / 2;
 
